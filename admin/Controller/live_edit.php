e4fdb7660be1f37cc0f67535e5d305bc6a6cc298
garrychandra <garryalexanderchandra97@gmail.com>
2025-07-21 20:38:23 +0700

    debug live_edit

--- admin/Controller/live_edit.php
+++ admin/Controller/live_edit.php
@@ -1,5 +1,6 @@
 <?php
 include_once("../Model/config.php");
+
 $input = filter_input_array(INPUT_POST);
 if ($input['action'] == 'edit') {	
 	$update_field='';
@@ -11,7 +12,8 @@ if ($input['action'] == 'edit') {
 		$update_field.= "harga='".$input['harga']."'";
     }
 	if($update_field && $input['produk_id']) {
-		$sql_query = "UPDATE produk SET $update_field WHERE produk_id='" . $input['produk_id'] . "'";	
+		$sql = "UPDATE produk SET $update_field WHERE produk_id='" . $input['produk_id'] . "'";	
+        mysqli_query($conn, $sql);
 		echo json_encode([
                 'status' => 'success',
                 'message' => 'Data berhasil diupdate'
@@ -19,7 +21,7 @@ if ($input['action'] == 'edit') {
         } else {
             echo json_encode([
                 'status' => 'error',
-                'message' => 'Database error: ' . $stmt->error
+                'message' => 'Database error: ' . mysqli_error($conn)
             ]);
         }
     mysqli_close($conn);
