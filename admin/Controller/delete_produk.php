2e952e051b5081d97edd42791cb7a38ec2a0f7f6
garrychandra <garryalexanderchandra97@gmail.com>
2025-07-21 20:29:42 +0700

    ubah-produk selesai

--- admin/Controller/delete_produk.php
+++ admin/Controller/delete_produk.php
@@ -1,8 +1,20 @@
 <?php
-//delete produk
+//delete gambar
 include_once "../Model/config.php";
+$sql = "SELECT * FROM gambar WHERE produk_id = ".$_POST['produk_id'].";";
+$result = mysqli_query($conn,$sql);
+if(mysqli_num_rows($result) > 0){
+    while($row = mysqli_fetch_assoc($result)){
+        $file_path = '../../uploads/'.$row['file'];
+        if(file_exists($file_path)){
+            unlink($file_path);
+        }
+    }
+}
+//delete PRODUK
 $sql = "DELETE FROM produk WHERE produk_id = ".$_POST['produk_id'].";";
 $result = mysqli_query($conn,$sql);
-mysqli_close($conn);
 
+
+mysqli_close($conn);
 ?>
