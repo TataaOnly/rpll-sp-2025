a8f75c344f69277beab604fdae596b8faeea43c2
Garry Alexander Chandra <garryalexanderchandra97@gmail.com>
2025-07-21 14:36:04 +0700

    Update plastik.sql

--- plastik.sql
+++ plastik.sql
@@ -2,10 +2,9 @@
 CREATE TABLE produk (
     produk_id INT AUTO_INCREMENT PRIMARY KEY,
     nama VARCHAR(255) NOT NULL,
-    deskripsi VARCHAR(255),
+    deskripsi TEXT,
     stok INT(255) DEFAULT 0,
     harga INT NOT NULL,
-    is_custom ENUM('Y', 'N') DEFAULT 'N',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status ENUM('Aktif', 'Non-Aktif') DEFAULT 'Aktif'
 );
@@ -13,8 +12,7 @@ CREATE TABLE produk (
 -- Table 2: Gambar (Images)
 CREATE TABLE gambar (
     gambar_id INT AUTO_INCREMENT PRIMARY KEY,
-    nama VARCHAR(255) NOT NULL,
-    file VARCHAR(500) NOT NULL COMMENT 'File path or filename',
+    file VARCHAR(255) NOT NULL COMMENT 'File path or filename',
     produk_id INT NOT NULL,
     FOREIGN KEY (produk_id) REFERENCES produk(produk_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -24,8 +22,9 @@ CREATE TABLE kontak (
     kontak_id INT AUTO_INCREMENT PRIMARY KEY,
     nama VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    no_telp VARCHAR(12),
-    no_wa VARCHAR(12),
+    no_telp VARCHAR(13),
+    no_wa VARCHAR(13),
     map VARCHAR(255) COMMENT 'Map coordinates or URL',
     alamat VARCHAR(255)
+    admin_pass varchar(255) NOT NULL
 );
