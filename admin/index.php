0d93ec98975023b6de67cfa2c263435c8321bda5
garrychandra <garryalexanderchandra97@gmail.com>
2025-07-26 22:47:16 +0700

    ubah-galeri done

--- admin/index.php
+++ admin/index.php
@@ -16,8 +16,6 @@ if(isset($_SESSION['login'])) {
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <img class="mx-auto h-auto w-auto" src="../images/logo.png" alt="PlastikHB" />
-    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Admin</h2>
-
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
